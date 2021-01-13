2b6c47a8476cd0f73f609082c4c756b020fb0377
Justine Lemesle <justine.lemesle2000@gmail.com>
2021-01-13 15:48:16 +0100

    début

--- PieceQuantik.php
+++ PieceQuantik.php
@@ -3,18 +3,20 @@
 
 class PieceQuantik
 {
-    public static int $WHITE = 0;
-    public static int $BLACK = 1;
-    public static int $VOID = 0;
-    public static int $CUBE = 1;
-    public static int $CONE = 2;
-    public static int $CYLINDRE = 3;
-    public static int $SPHERE = 4;
+    public const WHITE = 0;
+    public const BLACK = 1;
+    public const VOID = 0;
+    public const CUBE = 1;
+    public const CONE = 2;
+    public const CYLINDRE = 3;
+    public const SPHERE = 4;
     protected int $forme;
     protected int $couleur;
 
     //constructeur
     private function __construct(int $forme, int $couleur){
+        $this->forme = $forme;
+        $this->couleur = $couleur;
 
     }
     public function getForme(){
@@ -24,39 +26,39 @@ class PieceQuantik
         return $this->couleur;
     }
     public function __toString(){
-        return $this;
+
     }
     public static function initVoid(){
-
+        return new PieceQuantik(self::$VOID, self::$WHITE);
     }
 
     public static function initWhiteCube(){
-
+        return new PieceQuantik(self::$CUBE, self::$WHITE);
     }
 
     public static function initBlackCube(){
-
+        return new PieceQuantik(self::$CUBE,self::$BLACK);
     }
 
     public static function initWhiteCone(){
-
+        return new PieceQuantik(self::$CONE, self::$WHITE);
     }
 
     public static function initBlackCone(){
-
+        return new PieceQuantik(self::$CONE,self::$BLACK);
     }
 
     public static function initWhiteCylindre(){
-
+        return new PieceQuantik(self::$CYLINDRE, self::$WHITE);
     }
     public static function initBlackCylindre(){
-
+        return new PieceQuantik(self::$CYLINDRE, self::$BLACK);
     }
 
     public static function initWhiteSpere(){
-
+        return new PieceQuantik(self::$SPHERE, self::$WHITE);
     }
     public static function initBlackSphere(){
-
+        return new PieceQuantik(self::$SPHERE,self::$BLACK);
     }
 }
---
+++ PlateauQuantik.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class PlateauQuantik
+{
+ public const NBROWS = 4;
+ public const NBCOLS = 4;
+ public const NW = 0;
+ public const NE = 1;
+ public const SW = 2;
+ public const SE = 3;
+ protected  $cases;
+
+ public  function __construct(){
+     $this->cases = array(array(0,0,0,0),array(0,0,0,0),array(0,0,0,0),array(0,0,0,0));
+ }
+public function getPiece()
+}
